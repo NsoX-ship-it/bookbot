bf8157865332f3c964c2a18414c9c72d6fb611dc
NsoX-ship-it <nsox@live.co.uk>
2025-08-23 00:32:03 +0100

    bookbot script open and print

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(filepath):
+    
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+print(get_book_text("/home/nathan/boot/bookbot/books/frankenstein.txt"))
+
+
+    
