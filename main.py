453bbd934802b0dee1b09f035f5f3988a92596d4
NsoX-ship-it <nsox@live.co.uk>
2025-09-02 04:45:37 +0100

    main update with characer count

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
+def main():
+    book_filepath = "/home/nathan/boot/bookbot/books/frankenstein.txt"
+    book_text = get_book_text(book_filepath)
+    word_count = book_word_count(book_text)
+    print(f"{word_count} words found in the document")
+
 def get_book_text(filepath):
-    
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
+ 
+from stats import book_word_count 
 
-print(get_book_text("/home/nathan/boot/bookbot/books/frankenstein.txt"))
+main()
+
+
+
+
+
+
+
+
 
 
     
---
+++ stats.py
@@ -0,0 +1,4 @@
+def book_word_count(book_text):
+    book_words = book_text.split()
+    book_word_count = len(book_words)
+    return book_word_count
