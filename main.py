6caad3a2b8544b659d65fbf1435b9967e92c911c
NsoX-ship-it <nsox@live.co.uk>
2025-09-05 04:56:36 +0100

    book character count update

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+from stats import book_character_count
+from stats import book_word_count 
+
 def main():
     book_filepath = "/home/nathan/boot/bookbot/books/frankenstein.txt"
     book_text = get_book_text(book_filepath)
     word_count = book_word_count(book_text)
     print(f"{word_count} words found in the document")
+    character_count = book_character_count(book_text)
+    print(character_count)
 
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
  
-from stats import book_word_count 
-
 main()
 
 
--- stats.py
+++ stats.py
@@ -2,3 +2,15 @@ def book_word_count(book_text):
     book_words = book_text.split()
     book_word_count = len(book_words)
     return book_word_count
+
+def book_character_count(book_text):
+    character_count = {}
+    book_words = book_text.lower()
+    book_characters = list(book_words) 
+    for characters in book_characters:
+        if characters in character_count:
+            character_count[characters] += 1
+        else:
+            character_count[characters] = 1
+    return character_count
+    
